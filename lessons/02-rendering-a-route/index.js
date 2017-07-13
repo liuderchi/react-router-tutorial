5b0114102bcc67db04c68c6fd0c99bf13e9dd34b
Derek Liu <derek.liu@mediatek.com>
2017-07-13 15:02:24 +0800

    lec 2: observation of using bashHistory in Router

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import { Router, Route, hashHistory } from 'react-router'
+
+// render(<App/>, document.getElementById('app'))
+
+render((
+  <Router history={hashHistory}>
+    {/*
+      NOTE what's history?
+        - what you see: it becomes http://localhost:8080/#/?_k=cdlcqb
+        - it's bc we use bashHistory
+      */}
+    <Route path="/" component={App}/>
+  </Router>
+), document.getElementById('app'))
