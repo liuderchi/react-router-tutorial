62d8a082151875b6360ffa68c476e109fd44169d
Derek Liu <derek.liu@mediatek.com>
2017-07-13 15:09:27 +0800

    lec 2: add Route for different path, with complete match route

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
 import { Router, Route, hashHistory } from 'react-router'
-
-// render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
 
 render((
   <Router history={hashHistory}>
+    <Route path="/" component={App}/>
     {/*
       NOTE what's history?
         - what you see: it becomes http://localhost:8080/#/?_k=cdlcqb
         - it's bc we use bashHistory
       */}
-    <Route path="/" component={App}/>
+    {/* add the routes here */}
+    {/*  NOTE load component when path matches */}
+    <Route path="/repos" component={Repos}/>
+    <Route path="/about" component={About}/>
   </Router>
 ), document.getElementById('app'))
