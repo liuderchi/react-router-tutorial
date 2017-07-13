9d9b349ef5d9dc438a20f544c838b9436e7761ef
Derek Liu <derek.liu@mediatek.com>
2017-07-13 17:03:36 +0800

    lec 6: PLAY: let Repo component become child of Repos

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
@@ -7,11 +7,13 @@ export default React.createClass({
       <div>
         <h2>Repos</h2>
 
-        {/* add some links */}
         <ul>
-          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
-          <li><Link to="/repos/facebook/react">React</Link></li>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
         </ul>
+
+        {/* will render `Repo.js` when at /repos/:userName/:repoName */}
+        {this.props.children}
 
       </div>
     )
