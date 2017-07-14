c5a74f7b277c0bafa7000af877f7c42a522dec52
Derek Liu <derek.liu@mediatek.com>
2017-07-14 14:26:39 +0800

    lec 9: use onlyActiveOnIndex prop to let only root url can activate <Link/>

--- lessons/09-index-links/modules/App.js
+++ lessons/09-index-links/modules/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link, IndexLink } from 'react-router'
+
 import NavLink from './NavLink'
 
 export default React.createClass({
@@ -9,6 +11,16 @@ export default React.createClass({
         <ul role="nav">
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
+
+          <li><Link to="/" activeClassName="active" onlyActiveOnIndex={true}>Home</Link></li>
+          {/* NOTE become active only when path matches / */}
+
+          <li><IndexLink to="/" activeClassName="active">Home 2nd</IndexLink></li>
+          {/* NOTE built-in short-hand with onlyActiveOnIndex wrapped */}
+
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home 3rd</NavLink></li>
+          {/* NOTE inject prop into inner Link as 1st example */}
+
         </ul>
         {this.props.children}
       </div>
