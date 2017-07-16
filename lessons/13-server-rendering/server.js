ef862b7763426ed2636654b7ecb1211cbcfed0b3
Derek Liu <derek.liu@mediatek.com>
2017-07-16 18:55:43 +0800

    * lec 13: apply server rendering
    - interesting observation: server doesn't render <RouterContext> when url is /
    - remove index.html to prevent server send file directly
    
    - TODO send PR to propose to remove index.html in README.md

--- lessons/13-server-rendering/server.js
+++ lessons/13-server-rendering/server.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var path = require('path')
 var compression = require('compression')
+import React from 'react'
+// we'll use this to render our app to an html string
+import { renderToString } from 'react-dom/server'
+// and these to match the url to routes and then render
+import { match, RouterContext } from 'react-router'
+
+import routes from './modules/routes'
+
 
 var app = express()
 
@@ -8,11 +16,36 @@ app.use(compression())
 
 // serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, 'public')))
+// NOTE: when hitting / server sends index.html directly rather than exec .get(*)
 
 // send all requests to index.html so browserHistory works
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  // match the routes to the url
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // `RouterContext` is what the `Router` renders. `Router` keeps these
+    // `props` in its state as it listens to `browserHistory`. But on the
+    // server our app is stateless, so we need to use `match` to
+    // get these props before rendering.
+    const appHtml = renderToString(<RouterContext {...props}/>)
+
+    // dump the HTML into a template, lots of ways to do this, but none are
+    // really influenced by React Router, so we're just using a little
+    // function, `renderPage`
+    res.send(renderPage(appHtml))
+  })
 })
+
+function renderPage(appHtml) {    // NOTE ref: index.html
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App</title>
+    <link rel=stylesheet href=/index.css>
+    <div id=app>${appHtml}</div>
+    <script src="/bundle.js"></script>
+   `
+}
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
