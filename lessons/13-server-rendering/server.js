00da45ecf0a45f1dfb6c484610e4d124984ce508
Derek Liu <derek.liu@mediatek.com>
2017-07-16 23:02:22 +0800

    lec 13: add more conditioning to request handler
    - fix es5 syntax to es6

--- lessons/13-server-rendering/server.js
+++ lessons/13-server-rendering/server.js
@@ -1,37 +1,37 @@
-var express = require('express')
-var path = require('path')
-var compression = require('compression')
+import express from 'express'
+import path from 'path'
+import compression from 'compression'
 import React from 'react'
-// we'll use this to render our app to an html string
 import { renderToString } from 'react-dom/server'
-// and these to match the url to routes and then render
 import { match, RouterContext } from 'react-router'
 
 import routes from './modules/routes'
 
 
-var app = express()
+const app = express()
 
 app.use(compression())
 
-// serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, 'public')))
 // NOTE: when hitting / server sends index.html directly rather than exec .get(*)
 
-// send all requests to index.html so browserHistory works
 app.get('*', function (req, res) {
   // match the routes to the url
   match({ routes: routes, location: req.url }, (err, redirect, props) => {
-    // `RouterContext` is what the `Router` renders. `Router` keeps these
-    // `props` in its state as it listens to `browserHistory`. But on the
-    // server our app is stateless, so we need to use `match` to
-    // get these props before rendering.
-    const appHtml = renderToString(<RouterContext {...props}/>)
-
-    // dump the HTML into a template, lots of ways to do this, but none are
-    // really influenced by React Router, so we're just using a little
-    // function, `renderPage`
-    res.send(renderPage(appHtml))
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirect) {
+      // we haven't talked about `onEnter` hooks on routes, but before a
+      // route is entered, it can redirect. Here we handle on the server.
+      res.redirect(redirect.pathname + redirect.search)
+    } else if (props) {
+      // if we got props then we matched a route and can render
+      const appHtml = renderToString(<RouterContext {...props}/>)
+      res.send(renderPage(appHtml))
+    } else {
+      // no errors, no redirect, we just didn't match anything
+      res.status(404).send('Not Found')
+    }
   })
 })
 
@@ -47,7 +47,7 @@ function renderPage(appHtml) {    // NOTE ref: index.html
    `
 }
 
-var PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
 })
