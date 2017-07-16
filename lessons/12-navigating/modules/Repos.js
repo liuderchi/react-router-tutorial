fa0f0e624fb79bd815a017bdba8b9a09e016154f
Derek Liu <derek.liu@mediatek.com>
2017-07-16 16:03:42 +0800

    * lec 12: nav pgogramatically: add form template and default submit handler

--- lessons/12-navigating/modules/Repos.js
+++ lessons/12-navigating/modules/Repos.js
@@ -2,6 +2,14 @@ import React from 'react'
 import NavLink from './NavLink'
 
 export default React.createClass({
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    console.log(`go to url: ${path}`)
+    // TODO handle page navigation programatically
+  },
   render() {
     return (
       <div>
@@ -9,6 +17,14 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          {/* NOTE: add form*/}
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
